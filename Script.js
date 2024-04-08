55982c3d964cc3cb36243abbda799b41e265c5d0
RAVIVERMA6393 <147376604+RAVIVERMA6393@users.noreply.github.com>
2024-04-08 10:57:22 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,58 @@
+// Q.1 Add two numbers
+// let a=10;
+// let b=20;
+// console.log(a+b);
+
+
+// Q.2 valid function
+// let a = 5;
+// let b = 3;
+// if(a < 10 && a > b){
+//     console.log(true)
+// }else{
+//     console.log(false)
+// }
+
+
+// Q.3
+// let num1=20;
+// let num2=30;
+// let num=num1%10 == 0 && num2%10 == 0;
+// console.log(num);
+
+// let num3=num1%10 !=0 || num2%10 !== 0;
+// console.log(num3);
+
+// let num4=num1%10 == 0 || num2%10 == 0;
+// console.log(num4);
+
+// let num = 4567;
+// let num1 = parseInt(num/1000);
+// console.log(num1);
+
+// Q.5
+// let num = 4567;
+// let num1 = num%10;
+// console.log(num1);
+
+
+// Q.6 remainder
+// let num = 29;
+// let num1 = num % 2;
+// console.log(num1);
+
+
+// Q.7 multiply no.
+// let a = 5;
+// let b = 2;
+// console.log(a*b);
+
+
+// Q.8
+// let a = 100;
+// let b = 50;
+// let c = 20
+// sum = a + b + c;
+// console.log(sum);
+// avg = sum / 3;
+// console.log(avg);
